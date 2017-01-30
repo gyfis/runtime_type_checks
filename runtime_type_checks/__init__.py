f50b16e525f6a81a364149993ea64775e24e83df
Tomas Hromada <gyfis@seznam.cz>
2017-01-30 03:06:11 +0100

    modify type matching to not specify exact typing classes

--- runtime_type_checks/__init__.py
+++ runtime_type_checks/__init__.py
@@ -7,61 +7,59 @@ class RuntimeTypeCheckError(TypeError):
     pass
 
 
-_UNINSTANTIABLE = [typing.TypeVar, typing.UnionMeta, typing.TupleMeta, typing.CallableMeta]
-
-
 def _type_eq(value, value_type):
+
     value_type_class = value_type.__class__
-    if value_type_class in _UNINSTANTIABLE:
-        if value_type_class == typing.TypeVar:
-            type_var_constraints = value_type.__constraints__
+    value_type_info = str(value_type)
 
-            # empty TypeVar matches everything
-            if not type_var_constraints:
-                return True
+    if value_type_class == typing.TypeVar:
+        type_var_constraints = value_type.__constraints__
 
-            if not any(_type_eq(value, constraint)
-                       for constraint
-                       in type_var_constraints):
-                return False
+        # empty TypeVar matches everything
+        if not type_var_constraints:
             return True
 
-        if value_type_class == typing.TupleMeta:
-            if not isinstance(value, tuple):
-                return False
-
-            tuple_params = value_type.__tuple_params__
-            if not tuple_params:
-                return True
-
-            if not hasattr(value, '__iter__'):
-                return False
-
-            if len(value) != len(tuple_params):
-                return False
-
-            if not all(_type_eq(inner_value, inner_value_type)
-                       for inner_value, inner_value_type
-                       in zip(value, value_type.__tuple_params__)):
-                return False
-
-            return True
-
-        if value_type_class == typing.UnionMeta:
-            union_constraints = value_type.__union_params__
-
-            if not union_constraints:
-                return True
-
-            if not any(_type_eq(value, constraint)
-                       for constraint
-                       in union_constraints):
-                return False
-            return True
-
-    else:
-        if not isinstance(value, value_type):
+        if not any(_type_eq(value, constraint)
+                   for constraint
+                   in type_var_constraints):
             return False
+        return True
+
+    if str(typing.Tuple) in value_type_info:
+        if not isinstance(value, tuple):
+            return False
+
+        tuple_params = value_type.__tuple_params__
+        if not tuple_params:
+            return True
+
+        if not hasattr(value, '__iter__'):
+            return False
+
+        if len(value) != len(tuple_params):
+            return False
+
+        if not all(_type_eq(inner_value, inner_value_type)
+                   for inner_value, inner_value_type
+                   in zip(value, value_type.__tuple_params__)):
+            return False
+
+        return True
+
+    if str(typing.Union) in value_type_info:
+        union_constraints = value_type.__union_params__
+
+        if not union_constraints:
+            return True
+
+        if not any(_type_eq(value, constraint)
+                   for constraint
+                   in union_constraints):
+            return False
+        return True
+
+    if not isinstance(value, value_type):
+        return False
 
     return True
 
